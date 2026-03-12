685cbf8dbbbb9aa63d001d7787a53cf1f2181ed4
Md saaduddin shahbaz <141928636+Mdsaaduddinshahbaz@users.noreply.github.com>
2026-03-13 03:19:56 +0530

    Update server.py

--- server.py
+++ server.py
@@ -228,5 +228,5 @@ def savekey():
     except:
         return ({"success":False})
 if __name__ == "__main__":
-    # socketio.run(app, debug=True)
-    socketio.run(app, host="0.0.0.0" , port=5000 ,debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port, allow_unsafe_werkzeug=True)
